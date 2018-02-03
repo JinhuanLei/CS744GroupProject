258c1ffeedae742a7dffe082a4a619bd6d66cbb0
JinhuanLei <lei,jinhuan@uwlax.edu>
2018-02-03 17:56:06 -0600

    Adding initial unit tests for database

--- MonitorNetwork.Test/Database/TestDatabase.cs
+++ MonitorNetwork.Test/Database/TestDatabase.cs
@@ -29,10 +29,117 @@ namespace MonitorNetwork.Test
             }
         }
 
+
+
         [TestMethod]
-        public void TestDataEntry()
+        public void TestInsertAccount()
         {
-            string query = "INSERT INTO new_table (new_tablecol) VALUES('testvalue2')";
+            string query = "INSERT INTO account (accountFirstName,accountLastName,address,phoneNumber,spendingLimit,balance) VALUES('Jane','Zhou','1234 La St.','6084334881','3000','5000')";
+
+            MySqlConnection cnn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                cnn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cnn);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [TestMethod]
+        public void TestInsertCreditCard()
+        {
+            string query = "INSERT INTO creditcard(cardNumber,expirationDate,securityCode,customerFirstName,customerLastName,accID) VALUES('95001255923431','2017-12-20 12:30:30','125','Jane','Doe','2')";
+
+            MySqlConnection cnn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                cnn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cnn);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [TestMethod]
+        public void TestInsertRelay()
+        {
+            string query = "INSERT INTO relay(relayIP,status,isProcessingCenter) VALUES('192.168.1.1','1','1')";
+
+            MySqlConnection cnn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                cnn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cnn);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [TestMethod]
+        public void TestInsertRelay2()
+        {
+            string query = "INSERT INTO relay(relayIP,status,isProcessingCenter) VALUES('192.168.1.1','1','1')";
+
+            MySqlConnection cnn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                cnn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cnn);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        [TestMethod]
+        public void TestDeleteAccount()
+        {
+            string query = "DELETE FROM account WHERE accountID= 7 ";
+            //INSERT INTO account (accountFirstName,accountLastName,address,phoneNumber,spendingLimit,balance) VALUES('Jane','Zhou','1234 La St.','6084334881','3000','5000')
+
+            MySqlConnection cnn = new MySqlConnection(myConnectionString);
+
+            try
+            {
+                cnn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, cnn);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [TestMethod]
+        public void TestUpdateAccount()
+        {
+            string query = "Update account set balance=10000 where accountid=8";
+            //INSERT INTO account (accountFirstName,accountLastName,address,phoneNumber,spendingLimit,balance) VALUES('Jane','Zhou','1234 La St.','6084334881','3000','5000')
+
             MySqlConnection cnn = new MySqlConnection(myConnectionString);
 
             try
@@ -49,4 +156,7 @@ namespace MonitorNetwork.Test
             }
         }
     }
+
+   
 }
+
