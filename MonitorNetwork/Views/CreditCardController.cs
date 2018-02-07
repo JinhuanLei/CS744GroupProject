ef8d7d4c5f0ec63cbdd15d905a39b70c7c355127
Parker Riebs <prariebs@gmail.com>
2018-02-06 21:58:18 -0600

    Initial framework for adding/deleting/editing/viewing credit card information.

---
+++ MonitorNetwork/Views/CreditCardController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MonitorNetwork.Database;
+
+namespace MonitorNetwork.Views
+{
+    public class CreditCardController : Controller
+    {
+        private MNDatabase db = new MNDatabase();
+
+        // GET: CreditCard
+        public ActionResult Index()
+        {
+            var creditcard = db.creditcard.Include(c => c.account);
+            return View(creditcard.ToList());
+        }
+
+        // GET: CreditCard/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            creditcard creditcard = db.creditcard.Find(id);
+            if (creditcard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditcard);
+        }
+
+        // GET: CreditCard/Create
+        public ActionResult Create()
+        {
+            ViewBag.accountID = new SelectList(db.account, "accountID", "accountFirstName");
+            return View();
+        }
+
+        // POST: CreditCard/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cardID,cardNumber,expirationDate,securityCode,customerFirstName,customerLastName,accountID")] creditcard creditcard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.creditcard.Add(creditcard);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.accountID = new SelectList(db.account, "accountID", "accountFirstName", creditcard.accountID);
+            return View(creditcard);
+        }
+
+        // GET: CreditCard/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            creditcard creditcard = db.creditcard.Find(id);
+            if (creditcard == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.accountID = new SelectList(db.account, "accountID", "accountFirstName", creditcard.accountID);
+            return View(creditcard);
+        }
+
+        // POST: CreditCard/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cardID,cardNumber,expirationDate,securityCode,customerFirstName,customerLastName,accountID")] creditcard creditcard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(creditcard).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.accountID = new SelectList(db.account, "accountID", "accountFirstName", creditcard.accountID);
+            return View(creditcard);
+        }
+
+        // GET: CreditCard/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            creditcard creditcard = db.creditcard.Find(id);
+            if (creditcard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditcard);
+        }
+
+        // POST: CreditCard/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            creditcard creditcard = db.creditcard.Find(id);
+            db.creditcard.Remove(creditcard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
