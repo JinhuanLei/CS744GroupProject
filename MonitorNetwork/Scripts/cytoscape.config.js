9249bf7d3b968ab272f1ee929b6cab40787a2fb4
JinhuanLei <lei,jinhuan@uwlax.edu>
2018-03-04 19:37:21 -0600

    fix bugs for label

--- MonitorNetwork/Scripts/cytoscape.config.js
+++ MonitorNetwork/Scripts/cytoscape.config.js
@@ -45,8 +45,8 @@ var cy = cytoscape({
         })
         .selector('.newStyle')
         .css({
-            'background-color': '#000000',
-            'line-color': '#000000',
+            'background-color': '#8080ff',
+            'line-color': '#8080ff',
             'target-arrow-color': '#61bffc',
             'transition-property': 'background-color, line-color, target-arrow-color',
             'transition-duration': '0.5s'
@@ -89,61 +89,109 @@ var layout = cy.elements().layout({
     roots: '#r11'
 });
 layout.run();
-cy.on('mouseover', 'node', function (event) {
+//cy.on('mouseover', 'node', function (event) {
 
-    var nodeId = event.target.id();
-    var transactionIds = [];
-    nodeQueues[nodeId].forEach(function (queueTransaction) {
+//    var nodeId = event.target.id();
+//    var transactionIds = [];
+//    nodeQueues[nodeId].forEach(function (queueTransaction) {
+//        transactionIds.push(queueTransaction.transactionId);
+//    });
+//    var queueStr = transactionIds.join(", ");
+
+//    cy.elements("node[id^='s']").qtip({
+//        //content: 'node IP',
+//        content: nodeId + " " + queueStr,
+//        show: {
+//            event: event.type,
+//            // ready: true,
+//            solo: true
+//        },
+//        hide: {
+//            event: 'mouseout unfocus'
+//        },
+//        style: {
+//            classes: 'qtip-bootstrap',
+//            tip: {
+//                width: 16,
+//                height: 8
+//            }
+//        }
+//    }, event);
+
+
+//    cy.elements("node[id^='r']").qtip({
+//        //content: 'relay IP',
+//        content: nodeId + " " + queueStr,
+//        show: {
+//            event: event.type,
+//            // ready: true,
+//            solo: true
+//        },
+//        hide: {
+//            event: 'mouseout unfocus'
+//        },
+//        style: {
+//            classes: 'qtip-bootstrap',
+//            tip: {
+//                width: 16,
+//                height: 8
+//            }
+//        }
+//    }, event);
+
+//});
+cy.nodes().qtip({
+
+    content: function () {
+        var nodeId = this.data('id');
+        var label = this.data('label');
+        var transactionIds = [];
+            nodeQueues[nodeId].forEach(function (queueTransaction) {
         transactionIds.push(queueTransaction.transactionId);
     });
-    var queueStr = transactionIds.join(", ");
-
-    cy.elements("node[id^='s']").qtip({
-        //content: 'node IP',
-        content: nodeId + " " + queueStr,
-        show: {
-            event: event.type,
-            // ready: true,
-            solo: true
-        },
-        hide: {
-            event: 'mouseout unfocus'
-        },
-        style: {
-            classes: 'qtip-bootstrap',
-            tip: {
-                width: 16,
-                height: 8
+            var queueStr = transactionIds.join(", ");
+            if (nodeId[0] == "r") {
+                return '192.168.' + this.data('label') + '\n'+'Queue:' + queueStr
+            } else {
+                var merchantName;
+                stores.forEach(function (t, number, ts) {
+                    console.log(t.storeID + "   " + nodeId.substring(1));
+                    console.log(t.storeID == nodeId.substring(1));
+                    if (t.storeID==nodeId.substring(1)) {
+                      
+                        merchantName = t.merchantName;
+                    }
+                });
+                  return merchantName + ':192.168.' + label + '\n' + 'Queue:' + queueStr
             }
+    
+    },
+    position: {
+        my: 'top center',
+        at: 'bottom center'
+    },
+    style: {
+        classes: 'qtip-bootstrap',
+        tip: {
+            width: 16,
+            height: 8
         }
-    }, event);
-
-
-    cy.elements("node[id^='r']").qtip({
-        //content: 'relay IP',
-        content: nodeId + " " + queueStr,
-        show: {
-            event: event.type,
-            // ready: true,
-            solo: true
-        },
-        hide: {
-            event: 'mouseout unfocus'
-        },
-        style: {
-            classes: 'qtip-bootstrap',
-            tip: {
-                width: 16,
-                height: 8
-            }
+    },
+    show: {
+        event: 'mouseover',
+        solo: true,
+        effect: function (offset) {
+            $(this).slideDown(100); // "this" refers to the tooltip
         }
-    }, event);
 
+    },
+    hide: {
+        event: 'mouseout unfocus'
+    }
 });
 
-
 cy.on('click', 'node', function (evt) {
-   
+    changestyle(evt, "node");
     var nodeid = evt.target.id();
     var id = (Number)(nodeid.substring(1));
     //alert(evt.target.id() + "  " + id);
@@ -156,9 +204,10 @@ cy.on('click', 'node', function (evt) {
     console.log($(relays)); 
     })
 
-
+console.log(stores);
 cy.on('click', 'edge', function (evt) {
-    alert(evt.target.id());
+    //alert(evt.target.id());
+    changestyle(evt, "edge");
     var edgeid = evt.target.id();
     var start = "";
     var dest = "";
@@ -188,7 +237,44 @@ cy.on('click', 'edge', function (evt) {
 // cy.autolock( true );
 cy.userZoomingEnabled(false);
 
+function getStoreIpAdress(storeID) {
+    console.log(storeID);
+    var nodeid = storeID;
+    var id = (Number)(nodeid.substring(1));
+    //alert(evt.target.id() + "  " + id);
+    for (var x = 0; x < stores.length; x++) {
+        //  alert(relays[x].relayID + "  " + id);
+        if (stores[x].storeID == id) {
 
+            return stores[x].storeIP;
+        }
+    }
+}
+function getRelayIpAdress(relayID) {
+    console.log(relayID);
+    var nodeid = relayID;
+    var id = (Number)(nodeid.substring(1));
+    //alert(evt.target.id() + "  " + id);
+    for (var x = 0; x < relays.length; x++) {
+        //  alert(relays[x].relayID + "  " + id);
+        if (relays[x].relayID == id) {
+            return relays[x].relayIP;
+        }
+    }
+}
+
+function changestyle(evt, type) {
+    if (evt.target.id() != "R11") {
+
+
+        if (cy.$("#" + evt.target.id()).hasClass('newStyle') == false) {
+            cy.$("#" + evt.target.id()).classes('newStyle');
+        }
+        else {
+            cy.$("#" + evt.target.id()).classes(type);
+        }
+    }
+}
 
 
 
