10cceb7a60e5401e1649215ac64f48300212b62c
Parker Riebs <prariebs@gmail.com>
2018-03-20 13:04:10 -0500

    Added inputmask package.

--- MonitorNetwork/App_Start/BundleConfig.cs
+++ MonitorNetwork/App_Start/BundleConfig.cs
@@ -26,6 +26,17 @@ namespace MonitorNetwork
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/inputmask").Include(
+                    //~/Scripts/Inputmask/dependencyLibs/inputmask.dependencyLib.js",  //if not using jquery
+                    "~/Scripts/Inputmask/inputmask.min.js",
+                    "~/Scripts/Inputmask/jquery.inputmask.min.js",
+                    "~/Scripts/Inputmask/inputmask.extensions.min.js",
+                    "~/Scripts/Inputmask/inputmask.date.extensions.min.js",
+                    //and other extensions you want to include
+                    "~/Scripts/Inputmask/inputmask.numeric.extensions.min.js",
+                    "~/Scripts/Inputmask/inputmask.phone.extensions.min.js",
+                    "~/Scripts/Inputmask/phone-codes/phone.min.js"));
         }
     }
 }
