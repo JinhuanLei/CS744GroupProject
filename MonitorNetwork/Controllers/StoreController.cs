d82ee5a85741129aa4c24da7f04be8b00d8e24d6
Parker Riebs <prariebs@gmail.com>
2018-03-20 16:49:17 -0500

    Added Stores MVC page. Removed unneeded file.

---
+++ MonitorNetwork/Controllers/StoreController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MonitorNetwork.Database;
+
+namespace MonitorNetwork.Views
+{
+    public class StoreController : Controller
+    {
+        private MNDatabase db = new MNDatabase();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View(db.store.ToList());
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            store store = db.store.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // GET: Store/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Store/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "storeID,storeIP,merchantName")] store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.store.Add(store);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(store);
+        }
+
+        // GET: Store/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            store store = db.store.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        // POST: Store/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "storeID,storeIP,merchantName")] store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(store).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(store);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
