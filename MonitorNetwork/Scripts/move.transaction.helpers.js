247cec00598fdde2721d142526a2486154c4bf32
Parker Riebs <prariebs@gmail.com>
2018-03-21 20:54:58 -0500

    Added more node queue functionality. Refactored move.transaction javascript code.

---
+++ MonitorNetwork/Scripts/move.transaction.helpers.js
@@ -0,0 +1,87 @@
+﻿
+
+// Add transaction to an element queue.
+function addTransactionToElementQueue(elementId, transaction, timeoutObj) {
+
+    elementQueues[elementId].queue.push(transaction);
+
+    // Add timeouts to transaction for pausing and resuming.
+    transaction.timeoutObj = timeoutObj;
+}
+
+// Sends the transaction to its defined next element.
+function sendTransactionToElement(transaction) {
+    var transactionTimeout = transaction.timeout;
+    transactionTimeout.sendFunc(transactionTimeout.fromNode, transactionTimeout.toNode, transaction);
+}
+
+function queueIsPassedLimit(nodeId) {
+    return getQueueLength(nodeId) > elementQueues[nodeId].limit;
+}
+
+function getQueueLength(elementId) {
+    return elementQueues[elementId].queue.length;
+}
+
+function getOutOfQueueLimitTransaction(elementId) {
+    if (queueIsPassedLimit(elementId)) {
+        return elementQueues[elementId].queue[elementQueues[nodeId].limit];
+    }
+
+    return null;
+}
+
+function hasReachedDestination(currentNode, transaction) {
+    if (transaction.toProcCenter) {
+        return currentNode === processingCenterId;
+    } else {
+        return currentNode === transaction.storeId;
+    }
+}
+
+function getPath(currentNode, transaction) {
+    if (transaction.toProcCenter) {
+        return findPath(currentNode, processingCenterId, true);
+    } else {
+        return findPath(currentNode, transaction.storeId, false);
+    }
+}
+
+function highlightConnection(nodeId1, nodeId2, highlight) {
+    var highlightNodeId;
+
+    for (var i = 0; i < cytoscapeEdges.length; i++) {
+        if ((cytoscapeEdges[i].data.source === nodeId1 && cytoscapeEdges[i].data.target === nodeId2) ||
+            (cytoscapeEdges[i].data.source === nodeId2 && cytoscapeEdges[i].data.target === nodeId1)) {
+            highlightNodeId = cytoscapeEdges[i].data.source + cytoscapeEdges[i].data.target;
+        }
+    }
+    if (highlightNodeId === undefined) {
+        console.log("Could not find edge in graph for " + nodeId1 + " " + nodeId2);
+        return;
+    }
+    if (highlight) {
+        cy.$('#' + highlightNodeId).addClass('highlighted');
+    } else {
+        cy.$('#' + highlightNodeId).removeClass('highlighted');
+    }
+}
+
+function findConnectionId(nodeId1, nodeId2) {
+
+    if (!nodeId1 || !nodeId2) {
+        console.log("One of the nodes was undefined.");
+        return;
+    }
+
+    if (elementQueues[nodeId1 + nodeId2] !== undefined) {
+        return nodeId1 + nodeId2;
+    } else if (elementQueues[nodeId2 + nodeId1] !== undefined) {
+        return nodeId2 + nodeId1;
+    }
+
+    if (connectionId === undefined) {
+        console.log("Could not find connection for " + nodeId1 + " " + nodeId2);
+        return;
+    }
+}
---
+++ MonitorNetwork/Scripts/move.transaction.toconnection.js
@@ -0,0 +1,62 @@
+﻿//var processingCenterId;
+//var elementQueues = { "r1" : { type: "R", queue: [{ transactionId: 100001, toProcCenter = true, storeId: "s1", destinationReached: false, timeoutObj : {timeoutObj} }], limit : 10 };
+//var connectionQueues = { "s1r1" : { transactionId: 100001, toProcCenter = true, storeId: "s1", timeoutObj : {timeoutObj} } }
+//var timeoutObj = { timeout: func, sendFunc: func, fromNode: "r1", toNode: "s6" }
+
+
+function moveTransactionFromNodeToConnection(fromNode, toNode, connectionId, transaction) {
+    // Remove transaction from the node the transaction was just at.
+    elementQueues[fromNode].queue.shift();
+
+    highlightConnection(fromNode, toNode, true);
+
+    var connectionTimeout = setTimeout(sendToNode, MILLI_SECOND_MOVEMENT_SPEED, fromNode, toNode, transaction);
+
+    // Add transaction to connection.
+    addTransactionToElementQueue(connectionId, transaction, { timeout: connectionTimeout, sendFunc: sendToNode, fromNode: fromNode, toNode: toNode });
+}
+
+function sendToConnection(fromNode, toNode, transaction) {
+
+    // The node queue the transaction needs to go to is full.
+    if (queueIsPassedLimit(toNode)) {
+        // When the transaction head of the full queue shifts off, if this
+        // transaction is waiting on the full queue, and next in line
+        // it will start this transaction from it's fromNode location.
+        // allowing it to move off the fromNode stack on to the toNode.
+        addTransactionToElementQueue(connectionId, transaction, { timeout: null, sendFunc: sendToConnection, fromNode: fromNode, toNode: toNode });
+        return;
+    }
+
+    var connectionId = findConnectionId(fromNode, toNode);
+    // Check if there is a transaction all ready on the connection.
+    if (!queueIsPassedLimit(connectionId)) {
+        // No transaction on the connection
+
+        // Get transaction that is in the queue but are out of limits.
+        var queueLimitTransacion = getOutOfQueueLimitTransaction(elementId);
+
+        moveTransactionFromNodeToConnection(fromNode, toNode, connectionId, transaction);
+
+        if (queueLimitTransaction != null) {
+
+        }
+
+        if (getQueueLength(fromNode) > 0) {
+            // There is still transactions in the fromNode's queue, start the next one.
+            sendTransactionToElement(elementQueues[fromNode].queue[0]);
+
+        } else {
+            // No more transaction in the queue
+            // Remove highlighting for node.
+            cy.$('#' + fromNode).removeClass('highlighted');
+        }
+
+    } else {
+        // Transaction already on connection. Wait for transaction that
+        // is using the line to finish, and start this connection.
+
+        // Add the transaction to the connection queue.
+        addTransactionToElementQueue(connectionId, transaction, { timeout: null, sendFunc: sendToConnection, fromNode: fromNode, toNode: toNode });
+    }
+}
