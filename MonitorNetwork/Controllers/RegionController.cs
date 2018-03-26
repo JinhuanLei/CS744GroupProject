8cced863006f1d1fbcae3a38658269e38e1e8235
Parker Riebs <prariebs@gmail.com>
2018-03-26 13:54:24 -0500

    Removed unnecessary files. Fixed adding a region bugs.

--- MonitorNetwork/Controllers/RegionController.cs
+++ MonitorNetwork/Controllers/RegionController.cs
@@ -15,27 +15,6 @@ namespace MonitorNetwork.Views
     {
         private MNDatabase db = new MNDatabase();
 
-        // GET: Region
-        public ActionResult Index()
-        {
-            return View(db.region.ToList());
-        }
-
-        // GET: Region/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            region region = db.region.Find(id);
-            if (region == null)
-            {
-                return HttpNotFound();
-            }
-            return View(region);
-        }
-
 		// GET: Region/Create
 		public ActionResult Create()
 		{
@@ -53,7 +32,7 @@ namespace MonitorNetwork.Views
 					   join colors2 in db.colors on colors.colorID equals colors2.colorID
 					   select new { colors2.colorName, colors2.colorID };
 
-			ViewBag.colorID = new SelectList(temp, "colorID", "colorName");
+            ViewBag.region = new { colorID = new SelectList(temp, "colorID", "colorName") };
 
 			return View(regionStoreRelay);
 		}
@@ -66,14 +45,15 @@ namespace MonitorNetwork.Views
         {
             if (ModelState.IsValid)
             {
-				region newRegion = new region()
-				{
-					colorID = regionStoreRelay.region.colorID,
-					regionNumber = regionStoreRelay.region.regionID
+                region newRegion = new region()
+                {
+                    colorID = regionStoreRelay.region.colorID,
+                    regionNumber = db.region.Select(x => x.regionID).Max() + 1
 				};
-				db.region.Add(regionStoreRelay.region);
+				db.region.Add(newRegion);
 				db.store.Add(regionStoreRelay.store);
 				db.SaveChanges();
+
 
 				relay gatewayRelay = new relay()
 				{
@@ -82,19 +62,16 @@ namespace MonitorNetwork.Views
 					queueLimit = regionStoreRelay.relay.queueLimit,
 					isGateway = true,
 					relayIP = regionStoreRelay.relay.relayIP,
-					regionID = regionStoreRelay.relay.regionID
+					regionID = newRegion.regionID
 				};
 
 				db.relay.Add(gatewayRelay);
-				
-				
-				var gateways = db.relay.Where(x => x.isGateway || x.isProcessingCenter);
-                db.SaveChanges();
+				db.SaveChanges();
 
 				connections connection = new connections()
 				{
 					storeID = regionStoreRelay.store.storeID,
-					destRelayID = regionStoreRelay.relay.relayID,
+					destRelayID = gatewayRelay.relayID,
 					isActive = true,
 					weight = regionStoreRelay.connections.weight
 				};
@@ -107,7 +84,7 @@ namespace MonitorNetwork.Views
 				{
 					connections connection2 = new connections()
 					{
-						relayID = regionStoreRelay.relay.relayID,
+						relayID = gatewayRelay.relayID,
 						destRelayID = selectedRelay.relayID,
 						isActive = true,
 						weight = selectedRelay.weight
@@ -132,74 +109,8 @@ namespace MonitorNetwork.Views
 					   join colors2 in db.colors on colors.colorID equals colors2.colorID
 					   select new { colors2.colorName, colors2.colorID };
 
-			ViewBag.colorID = new SelectList(temp, "colorID", "colorName");
+			ViewBag.region = new { colorID = new SelectList(temp, "colorID", "colorName") };
 			return View(regionStoreRelay);
-        }
-
-		[HttpGet]
-		public ActionResult GetRelays(int regionId)
-		{
-			return PartialView("_GatewayPartial", new RegionStoreRelayModel()
-			{
-				CheckboxGatewayModel = GetGatewayRelays()
-			});
-		}
-
-		// GET: Region/Edit/5
-		public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            region region = db.region.Find(id);
-            if (region == null)
-            {
-                return HttpNotFound();
-            }
-            return View(region);
-        }
-
-        // POST: Region/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "regionID,regionNumber")] region region)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(region).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(region);
-        }
-
-        // GET: Region/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            region region = db.region.Find(id);
-            if (region == null)
-            {
-                return HttpNotFound();
-            }
-            return View(region);
-        }
-
-        // POST: Region/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            region region = db.region.Find(id);
-            db.region.Remove(region);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
 		private IList<CheckboxGatewayModel> GetGatewayRelays()
