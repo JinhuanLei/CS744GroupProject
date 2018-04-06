99a96850941be4313e3d12c9769fbfd768db98b6
Maddie Sigl <maddiesigl@gmail.com>
2018-04-06 09:05:18 -0500

    in progress - relay creation page

---
+++ MonitorNetwork/Controllers/RelayController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MonitorNetwork.Database;
+using MonitorNetwork.Models;
+
+namespace MonitorNetwork.Views
+{
+    public class RelayController : Controller
+    {
+        private MNDatabase db = new MNDatabase();
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            var relay = db.relay.Include(s => s.region);
+            return View(relay.ToList());
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            relay relay = db.relay.Find(id);
+            if (relay == null)
+            {
+                return HttpNotFound();
+            }
+            return View(relay);
+        }
+
+        // GET: Store/Create
+        public ActionResult Create()
+        {
+            var selectList = from region in db.region
+                             select new { regionID = region.regionID, regionColor = region.colors.colorName };
+
+            ViewBag.relay = new { regionID = new SelectList(selectList, "regionID", "regionColor") };
+            RelayModel storeModel = new StoreModel();
+
+            storeModel.checkboxRelayModel = GetCheckboxRelays(db.region.First().regionID);
+            return View(storeModel);
+        }
+
+        // POST: Store/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Create(StoreModel storeModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.store.Add(storeModel.store);
+
+                db.SaveChanges();
+
+                var selectedRelays = storeModel.checkboxRelayModel.Where(x => x.selected);
+                foreach (var selectedRelay in selectedRelays)
+                {
+                    connections connection = new connections()
+                    {
+                        storeID = storeModel.store.storeID,
+                        destRelayID = selectedRelay.relayID,
+                        isActive = true,
+                        weight = selectedRelay.weight
+                    };
+
+                    db.connections.Add(connection);
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Store");
+            }
+
+            storeModel.checkboxRelayModel = GetCheckboxRelays(storeModel.store.regionID);
+
+            var selectList = from region in db.region
+                             select new { regionID = region.regionID, regionColor = region.colors.colorName };
+
+            ViewBag.store = new { regionID = new SelectList(selectList, "regionID", "regionColor") };
+            return View(storeModel);
+        }
+
+        [HttpGet]
+        public ActionResult GetRelays(int regionId)
+        {
+            return PartialView("_RelayPartial", new StoreModel() {
+                checkboxRelayModel = GetCheckboxRelays(regionId)
+            });
+        }
+
+        private IList<CheckboxRelayModel> GetCheckboxRelays(int regionId)
+        {
+            var region = db.region.FirstOrDefault(x => x.regionID == regionId);
+
+            return (from relay in region.relay
+                    where !relay.isProcessingCenter
+                   select new CheckboxRelayModel
+                   {
+                       selected = false,
+                       relayIP = relay.relayIP,
+                       relayID = relay.relayID
+                   }).ToList();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MonitorNetwork/Models/RelayModel.cs
@@ -0,0 +1,15 @@
+﻿using MonitorNetwork.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonitorNetwork.Models
+{
+    public class RelayModel
+    {
+        public relay relay { get; set; }
+
+        public IList<CheckboxRelayModel> checkboxRelayModel { get; set; }
+    }
+}
